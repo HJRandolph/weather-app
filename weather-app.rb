4dc279a4e4772dbc84d37b1a6315c41b8fba7ee5
HJRandolph <hjrandolph@gmail.com>
2016-04-05 23:31:58 -0400

    First commit

---
+++ weather-app.rb
@@ -0,0 +1,33 @@
+require 'barometer'
+
+puts 'Please enter your zip code, postal code, or city and state/province.'
+area = gets
+area = area.chomp
+
+puts "The location you entered was " + area
+
+#puts 'Choose your preferred temperature type: c or f'
+
+#type = gets
+#type = type.chomp
+
+
+barometer = Barometer.new(area)
+weather = barometer.measure
+puts weather.tomorrow.high.f
+	ans = 'The current temperature in ' + area + ' is ' + weather.current.temperature.f + 'F' + ' Tomorrow the high will be ' + weather.tomorrow.high.f + 'F.'
+
+require 'rubygems'
+require 'twilio-ruby'
+
+account_sid = "ACf2c1bfb41e3119dd2ec8f614917cfeee"
+auth_token = "ff0e7bbb8af0adb7998a52ab7185b304"
+
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+message = @client.account.messages.create(
+:from => "+1 857-576-2043",
+:to => "+1 781-771-8098",
+:body => ans
+)
+
+puts message.to
